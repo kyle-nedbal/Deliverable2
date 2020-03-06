051c69687dfe5242d979a168004c27d93cab88bf
kyle-nedbal <kylenedbal88@gmail.com>
2020-03-06 18:41:13 -0500

    First commit for deliverable2

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int checksum = 0;
+            string input = "";
+            string message = "";
+
+            Console.WriteLine("Input your message.");
+            input = Console.ReadLine().ToUpper();
+
+            foreach (char letter in input)
+            {
+                string messageLetter = (letter - 64).ToString();
+                message += messageLetter + "-";
+                checksum += letter;
+            }
+
+            Console.WriteLine($"Encoded message is {message}");
+            Console.WriteLine($"Message checksum is {checksum}");
+            Console.ReadLine();
+        }
+    }
+}
